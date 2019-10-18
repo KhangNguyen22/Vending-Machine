d110f9504ac89c3f92d83f90527156770151f3f3
mngu3698 <mngu3698@uni.sydney.edu.au>
2019-10-18 15:58:43 +1100

    Added some dummy values inside Drinks class, added functionality of list out commands and list out current products in vending machine

--- src/main/java/Drinks.java
+++ src/main/java/Drinks.java
@@ -1,3 +1,12 @@
 public enum Drinks implements Snacks {
+    Water(10),
+    SoftDrink(10),
+    Juice(10);
+
+    private double price;
+    Drinks(double price)
+    {
+        this.price = price;
+    }
 
 }
