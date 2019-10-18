693dbd934dbdb01c3388dc0b1a2290493093209a
mngu3698 <mngu3698@uni.sydney.edu.au>
2019-10-18 16:26:41 +1100

    Added Drinks and list commands and options out in vending machine

--- src/main/java/VendingMachine.java
+++ src/main/java/VendingMachine.java
@@ -5,12 +5,41 @@ import java.util.Scanner;
 
 public class VendingMachine {
 
+    // HashMap of snack items and respective quantity
+    private HashMap<Snacks, Integer> snacks;
+
     // boolean for whether the user is an admin
     private boolean isAdmin;
 
     // Default max amount
     public VendingMachine(int amount) {
+        snacks = new HashMap<>();
+        snacks.put(Drinks.SoftDrink, amount);
         isAdmin = false;
+    }
+
+    /*
+     * List out the current items in stock with quantity and prices
+     * */
+
+    private String listItems(){
+        StringBuilder builder = new StringBuilder();
+        int i = 1;
+        for (Snacks snack : snacks.keySet()) {
+            builder.append(i++ + ". ").
+                    append(snack).
+                    append(" | Price: ").
+
+                    // Need to unHardCode This !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                            append(9999999);
+        }
+        builder.append("\n");
+        return builder.toString();
+    }
+
+    // Returns options of commands available
+    private String listOptions(){
+        return "1. List items\n" + "2. Buy Item\n" + "3. Staff Login\n" + "4. Exit\n" + "5. Staff Options";
     }
 
 
@@ -24,7 +53,7 @@ public class VendingMachine {
      *
      */
     public static void main(String[] args) {
-
+        boolean flag = true;
         if (args.length != 0) {
             return;
         }
@@ -32,9 +61,10 @@ public class VendingMachine {
         VendingMachine vendingMachine = new VendingMachine(10);
         Scanner scan = new Scanner(System.in);
 
-        while(scan.hasNext()) {
+        while(flag) {
 
             System.out.println("Please enter the number corresponding to the option you want.\n");
+            System.out.println(vendingMachine.listOptions());
             String line = scan.nextLine();
             int option;
             try {
@@ -49,10 +79,9 @@ public class VendingMachine {
 
                 // List Items
                 case 1:
-                    System.out.println("Items available: ");
+                    System.out.println("Items available");
                     System.out.println(vendingMachine.listItems());
                     break;
-
 
                 case 3:
                     if (vendingMachine.isAdmin()) {
