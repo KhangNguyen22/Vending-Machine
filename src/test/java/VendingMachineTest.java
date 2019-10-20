55921fba87a473d7cd57819f7846700d2e25eb70
AdamG <windows8@localhost.localdomain>
2019-10-20 15:53:26 +1100

    Fixed Jacoco report generation with build.gradle and test file

--- src/test/java/VendingMachineTest.java
+++ src/test/java/VendingMachineTest.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
+import org.junit.Test;
 
 public class VendingMachineTest {
+
+    // Assert that the program returns after receiving arguments
+    @Test
+    public void testVendingMachineMultipleArguments() {
+        VendingMachine.main(new String[]{"test1"});
+
+    }
 }
