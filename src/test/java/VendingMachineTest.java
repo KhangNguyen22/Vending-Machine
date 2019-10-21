77a080ee00748b71f4f35de51209cdbf7ef2bab8
Cameron Williamson <cwilliamson@transformd.com>
2019-10-21 17:50:36 +1100

    test commit to clean

--- src/test/java/VendingMachineTest.java
+++ src/test/java/VendingMachineTest.java
@@ -1,6 +1,13 @@
-import org.junit.Test;
+import org.junit.*;
+import static org.junit.Assert.*;
+
 
 public class VendingMachineTest {
+
+    @Before
+    public void setUp() {
+        VendingMachine vendingMachine = new VendingMachine(10);
+    }
 
     // Assert that the program returns after receiving arguments
     @Test
@@ -8,4 +15,18 @@ public class VendingMachineTest {
         VendingMachine.main(new String[]{"test1"});
 
     }
+
+    @Test
+    public void testListPayments() {
+        String expected = "1. $20\n" +
+                "2. $10\n" +
+                "3. $5\n" +
+                "4. $2\n" +
+                "5. $1\n" +
+                "6. 50c\n" +
+                "7. 20c\n" +
+                "8. 10c\n";
+        assertTrue("", expected.equals(vendingMachine.listPayments()));
+    }
+
 }
